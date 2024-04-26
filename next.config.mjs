6a10f349a001cbbdf755b87a43bf03b8d1284525
tk20211228 <t3kuboki@gmail.com>
2024-04-26 13:54:44 +0900

    added fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "demo-death-note",
+    // basePath: "/",
     images: {
         unoptimized: true,
     },
